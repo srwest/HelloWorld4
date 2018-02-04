2f7108090d7800c84b776b083c3c9f06e5db2068
srwest <spencer.west@maine.edu>
2018-02-04 12:33:01 -0500

    charlie fix

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,12 +5,7 @@ public class HelloWorld
 		System.out.println("Here ye : important announcement");
 		System.out.println("Hello, World!");
 		System.out.println("goodbye");
-<<<<<<< HEAD
-    System.out.println("Hi to both Alice and Bob");
-	System.out.println("hello to Denise");
-=======
 		System.out.println("Hi to both Alice and Bob");
-		System.out.println("hi to Charlie");
->>>>>>> CharlieBranch
+		System.out.println("hi to Charlie and Denise");
 	}
 }
