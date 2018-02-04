9b878ba7911c322f50350df1eb9849ee71c1eef8
srwest <spencer.west@maine.edu>
2018-02-04 12:28:18 -0500

    denise wuz here

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld
 		System.out.println("Hello, World!");
 		System.out.println("goodbye");
     System.out.println("Hi to both Alice and Bob");
+	System.out.println("hello to Denise");
 	}
 }
