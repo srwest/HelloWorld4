64bff2d1c3aac65879f688165d9b75e053be4f8b
srwest <31323196+srwest@users.noreply.github.com>
2018-02-08 12:50:21 -0500

    Add files via upload
    date and free memory statements work

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,7 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class HelloWorld
 {
 	public static void main (String[] args)
@@ -7,5 +11,10 @@ public class HelloWorld
 		System.out.println("goodbye");
 		System.out.println("Hi to both Alice and Bob");
 		System.out.println("hi to Charlie and Denise");
+		
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		System.out.println("Todays date: "+dateFormat.format(date));
+		System.out.println("Free memory (bytes) = "+Runtime.getRuntime().freeMemory());
 	}
 }
